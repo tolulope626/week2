72ab384d8058744386adc753c6b6950dde8930a4
Tolulope Egbetokun <tegbetokun@gmail.com>
2021-01-28 19:38:05 -0500

    Finished Constructor

--- week2.cpp
+++ week2.cpp
@@ -1,8 +1,27 @@
 
 #include<iostream>
+using namespace std;
+namespace cst8219 {
+	class Vehicle {
+	private:
+		int numWheels, numDoors;
 
+	public:
+		Vehicle() : Vehicle(4) {}
+
+		Vehicle(int w) : Vehicle(w, 4) {
+			numWheels = w;
+		}
+
+		Vehicle(int w, int d) {
+			numWheels = w;
+			numDoors = d;
+		}
+
+	};
+}
 int main(int argc, char **argv)
 {
-   std::cout << "Hello World!" << std::endl;
+   cout << "Hello World!" << endl;
    return 0;
 }
