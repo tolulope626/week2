584d6db44a4ec4659197a77dfa10ca91b94447fc
Tolulope Egbetokun <tegbetokun@gmail.com>
2021-01-28 19:44:30 -0500

    Finished Deconstructor

--- week2.cpp
+++ week2.cpp
@@ -7,17 +7,24 @@ namespace cst8219 {
 		int numWheels, numDoors;
 
 	public:
-		Vehicle() : Vehicle(4) {}
+		Vehicle() : Vehicle(4) {
+			cout << "In constructor with 0 parameters" << endl;
+		}
 
 		Vehicle(int w) : Vehicle(w, 4) {
 			numWheels = w;
+			cout << "In constructor with 1 parameters" << endl;
 		}
 
 		Vehicle(int w, int d) {
 			numWheels = w;
 			numDoors = d;
+			cout << "In constructor with 2 parameters" << endl;
 		}
 
+		~Vehicle(){
+			cout << "In Deconstructor" << endl;
+		}
 	};
 }
 int main(int argc, char **argv)
